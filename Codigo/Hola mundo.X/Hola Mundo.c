a94098bc1a9f9851fcb52a4731457c6a42aeebbb
Borxo García <borxoloko@live.com>
2020-09-09 18:29:37 +0200

    Subir

---
+++ Codigo/Hola mundo.X/Hola Mundo.c
@@ -0,0 +1,15 @@
+/*
+ * File:   Hola Mundo.c
+ * Author: borxo
+ *
+ * Created on 9 de septiembre de 2020, 12:45
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
